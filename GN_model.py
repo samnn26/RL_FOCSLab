f712cee90fb38f51b3e2ce08c0dd55a4db282f0c
Josh Nevin <jn399@cam.ac.uk>
2021-11-24 17:51:01 +0000

    modified to calculation per span

--- GN_model.py
+++ GN_model.py
@@ -6,24 +6,28 @@ def db_to_lin(x):
 def lin_to_db(x):
     return 10*np.log10(x)
 
-def calculate_etaunif_nikita(n_span,gamma,l_eff,beta2,r_sym,alpha_lin,n_ch):
-    '''
-    calculate eta unif in [1/W^2/ch.] as defined by Nikita (agreed with Seb)
-    '''
-    return 1e6*(8/27)*((n_span*(gamma**2)*alpha_lin*(l_eff**2))/(np.pi*beta2*(r_sym**2)))*np.arcsinh(1e-6*(((np.pi**2)*beta2*(n_ch**2)*(r_sym**2))/(2*alpha_lin)))
 
-def calculate_sig_ase_nikita(n_span,gain_lin,nf_lin,f_centre,r_sym):
-    h_p = 6.6256*1e-34   # [J.s] Planck's constant
-    return n_span*(gain_lin-1)*nf_lin*h_p*f_centre*r_sym*1e9
+def calculate_etaunif_nikita(gamma,l_eff,beta2,r_sym,alpha_lin,n_ch):
+    '''
+    calculate NLI noise coefficient (eta_unif) in [1/W^2/ch./span] 
+    '''
+    return 1e6*(8/27)*((gamma**2)*alpha_lin*(l_eff**2)/(np.pi*beta2*(r_sym**2)))*np.arcsinh(1e-6*(((np.pi**2)*beta2*(n_ch**2)*(r_sym**2))/(2*alpha_lin)))
+   
+def calculate_sig_ase_nikita(gain_lin,nf_lin,f_centre,r_sym):
+    '''
+    calculate ASE noise power per ch. per span in [W/ch./span] 
+    '''
+    h_p = 6.6256*1e-34   # [J.s] Planck's constant 
+    return (gain_lin-1)*nf_lin*h_p*f_centre*r_sym*1e9
 
-def calculate_throughput(r_sym, sig_ase, eta_unif):
+def calculate_throughput(n_span,r_sym, sig_ase, eta_unif):
     '''
     calculate throughput in [Tbps]
     '''
-    return 1e-3*2*r_sym*np.log2(1+(1/3)*((4/(sig_ase**2*eta_unif))**(1/3)))
+    return 1e-3*2*r_sym*np.log2(1+(1/3/n_span)*((4/(sig_ase**2*eta_unif))**(1/3)))
 
-def calculate_max_snr(sig_ase, eta_unif):
+def calculate_max_snr(n_span,sig_ase, eta_unif):
     '''
     calculate the maximum SNR
     '''
-    return (1/3)*((4/(sig_ase**2*eta_unif))**(1/3))
+    return (1/3/n_span)*((4/(sig_ase**2*eta_unif))**(1/3))
