9e4588d30a5b20033bf1dbe60ea90eca7d55b97e
Josh Nevin <jn399@cam.ac.uk>
2021-12-06 15:59:26 +0000

    added in heuristic to use existing lightpaths

--- optical_rl_gym/envs/rwa_env_focs.py
+++ optical_rl_gym/envs/rwa_env_focs.py
@@ -122,20 +122,39 @@ class RWAEnvFOCS(OpticalNetworkEnv):
             # breakpoint()
             for wavelen in range(self.num_spectrum_resources): # need to search each wavelength on each path
                 if self.is_path_free(self.k_shortest_paths[self.service.source, self.service.destination][kpath], wavelen):
-                    breakpoint()
+
     # if the path is free, then check the capacity - for now we can assume this is first-fit (another agent could even choose this?)
     # source,dest,path_id,channel_id
-
-                
                     if self.get_available_lightpath_capacity(self.service.source, self.service.destination, kpath, wavelen) > self.service.bit_rate:
 
                         # if there is enough capacity - provision path
                         self._provision_path(self.k_shortest_paths[self.service.source, self.service.destination][kpath], wavelen)
                         # need to exit this loop once request is provisioned
-                        request_accepted = True
-                        breakpoint()
+                        self.service.accepted = True
+                        self.services_accepted += 1
+                        self.episode_services_accepted += 1
+
+                        self.actions_taken[path, wavelength] += 1
+                        self.episode_actions_taken[path, wavelength] += 1
+                        self._add_release(self.service)
+                        self.services_processed += 1
+                        self.episode_services_processed += 1
+
+                        self.topology.graph['services'].append(self.service)
+                        info = {
+                            'service_blocking_rate': (self.services_processed - self.services_accepted) / self.services_processed,
+                            'episode_service_blocking_rate': (self.episode_services_processed - self.episode_services_accepted) / self.episode_services_processed,
+                            'path_action_probability': np.sum(self.actions_output, axis=1) / np.sum(self.actions_output),
+                            'wavelength_action_probability': np.sum(self.actions_output, axis=0) / np.sum(self.actions_output)
+                        }
+
+                        self._new_service = False
+                        self._next_service()
+                        return self.observation(), 0, self.episode_services_processed == self.episode_length, info
+                        # for now return a reward of 0 if an existing lightpath is used to service to the request (n.b. agent has no choice
+                        # if there is a viable one)
         """
-        Steps 6 onwards in Algorithm 1
+        Steps 6 onwards in Algorithm 1 - if no existing viable lightpath...
         """
         path, wavelength = action[0], action[1]
         self.actions_output[path, wavelength] += 1
