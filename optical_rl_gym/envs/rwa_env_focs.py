5892a4206b88c0e561810be49116a61f64ad783c
Sam N <sheramy21@gmail.com>
2021-12-14 15:16:13 +0000

    number of bit rates updated

--- optical_rl_gym/envs/rwa_env_focs.py
+++ optical_rl_gym/envs/rwa_env_focs.py
@@ -72,8 +72,8 @@ class RWAEnvFOCS(OpticalNetworkEnv):
         Again I don't understand the 10 here - what are the 10 things in the space here?
         """
         nodes = self.topology.number_of_nodes()
-        bitrate = 25 # test
-        self.observation_space= gym.spaces.MultiDiscrete((bitrate,nodes,nodes))
+        number_of_bitrates = 100 # test
+        self.observation_space= gym.spaces.MultiDiscrete((number_of_bitrates,nodes,nodes))
         self.action_space.seed(self.rand_seed)
         self.observation_space.seed(self.rand_seed)
 
