93577afafec0dc9d715aa4fe46c4a829998307fe
JoshWNevin <jn399@cam.ac.uk>
2022-03-02 09:40:32 +0000

    add topologies

--- heuristic_tests_v2.py
+++ heuristic_tests_v2.py
@@ -12,15 +12,25 @@ from optical_rl_gym.heuristics import kSP_FF, FF_kSP, kSP_MU, CA_MU
 
 
 current_directory = os.getcwd()
-with open(current_directory+'/topologies/nsfnet_chen_5-paths_rounded.h5', 'rb') as f:
+# with open(current_directory+'/topologies/nsfnet_chen_5-paths_rounded.h5', 'rb') as f:
+#     topology_5 = pickle.load(f)
+# with open(current_directory+'/topologies/nsfnet_chen_4-paths_rounded.h5', 'rb') as f:
+#     topology_4 = pickle.load(f)
+# with open(current_directory+'/topologies/nsfnet_chen_3-paths_rounded.h5', 'rb') as f:
+#     topology_3 = pickle.load(f)
+# with open(current_directory+'/topologies/nsfnet_chen_2-paths_rounded.h5', 'rb') as f:
+#     topology_2 = pickle.load(f)
+# with open(current_directory+'/topologies/nsfnet_chen_1-paths_rounded.h5', 'rb') as f:
+#     topology_1 = pickle.load(f)
+with open(current_directory+'/topologies/dtag_5-paths.h5', 'rb') as f:
     topology_5 = pickle.load(f)
-with open(current_directory+'/topologies/nsfnet_chen_4-paths_rounded.h5', 'rb') as f:
+with open(current_directory+'/topologies/dtag_4-paths.h5', 'rb') as f:
     topology_4 = pickle.load(f)
-with open(current_directory+'/topologies/nsfnet_chen_3-paths_rounded.h5', 'rb') as f:
+with open(current_directory+'/topologies/dtag_3-paths.h5', 'rb') as f:
     topology_3 = pickle.load(f)
-with open(current_directory+'/topologies/nsfnet_chen_2-paths_rounded.h5', 'rb') as f:
+with open(current_directory+'/topologies/dtag_2-paths.h5', 'rb') as f:
     topology_2 = pickle.load(f)
-with open(current_directory+'/topologies/nsfnet_chen_1-paths_rounded.h5', 'rb') as f:
+with open(current_directory+'/topologies/dtag_1-paths.h5', 'rb') as f:
     topology_1 = pickle.load(f)
 # node probabilities from https://github.com/xiaoliangchenUCD/DeepRMSA/blob/6708e9a023df1ec05bfdc77804b6829e33cacfe4/Deep_RMSA_A3C.py#L77
 # node_request_probabilities = np.array([0.01801802, 0.04004004, 0.05305305, 0.01901902, 0.04504505,
@@ -28,8 +38,8 @@ with open(current_directory+'/topologies/nsfnet_chen_1-paths_rounded.h5', 'rb') 
 #        0.07607608, 0.12012012, 0.01901902, 0.16916917])
 node_request_probabilities = pickle.load(open("node_req_probs/14_node_uniform.pkl",'rb'))
 load = int(1e10)
-ep_len = 20000
-req_br = 100e9
+ep_len = 2000
+req_br = 500e9
 tfb = False
 # mean_service_holding_time=7.5,
 env_args_1 = dict(topology=topology_1, seed=10, load = load,
@@ -73,8 +83,8 @@ env_5 = gym.make('RWAFOCS-v22', **env_args_5)
 print('RWAFOCS-v22')
 
 # heuristic = kSP_FF
-# heuristic = FF_kSP
-heuristic = kSP_MU
+heuristic = FF_kSP
+# heuristic = kSP_MU
 # heuristic = CA_MU
 
 # mean_reward_1, std_reward_1 = evaluate_heuristic(env_1, heuristic, n_eval_episodes=1,
